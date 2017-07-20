48c7c15b756b00816c0250333ebc237862730b29
Glaicon <gjpeixer@gmail.com>
2017-07-19 23:51:53 -0400

    GP: Alterado o CandidatoControllerTeste, implementação estava pela metade.

--- app/BackEnd/BackEnd.Teste/Unidade/CandidatoControllerTeste.cs
+++ app/BackEnd/BackEnd.Teste/Unidade/CandidatoControllerTeste.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System;
+using AutoMapper;
 using BackEnd.Controllers;
 using BackEnd.Data;
 using BackEnd.Model;
@@ -7,19 +8,37 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System.Collections.Generic;
+using System.Linq;
+using BackEnd.ViewModel;
+using FluentAssert;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Routing;
 using Xunit;
 
 namespace BackEnd.Teste.Unidade
 {
     public class CandidatoControllerTeste
     {
+        private readonly CandidatoController controller;
+        private readonly Mock<ICandidatoRepository> candidatoRepository;
+
+        public CandidatoControllerTeste()
+        {
+            AutoMapperConfiguration.Configure();
+
+            candidatoRepository = new Mock<ICandidatoRepository>();
+            var candidatoService = new Mock<CandidatoService>(candidatoRepository.Object);
+            var actionContext = ObterActionContext();
+            controller = new CandidatoController(Mapper.Instance, candidatoService.Object)
+            {
+                ControllerContext = new ControllerContext(actionContext)
+            };
+        }
+
         [Fact]
         public void deve_obter_todos_os_candidatos()
         {
-            Mock<IMapper> mapper = new Mock<IMapper>();
-            Mock<ICandidatoRepository> candidatoRepository = new Mock<ICandidatoRepository>();
-            Mock<CandidatoService> candidatoService = new Mock<CandidatoService>(candidatoRepository.Object);
-
             var candidatosNoBanco = new List<Candidato>
             {
                 new Candidato() {Id = 1},
@@ -32,24 +51,28 @@ namespace BackEnd.Teste.Unidade
             candidatoRepository.Setup(x => x.GetAll()).Returns(candidatosNoBanco);
             candidatoRepository.Setup(x => x.Count()).Returns(candidatosNoBanco.Count);
 
+            var resultado = controller.Get() as OkObjectResult;
+            var candidatosResumido = ((List<CandidatoResumidoViewModel>)resultado.Value);
+            candidatosResumido.Count().ShouldBeEqualTo(4);
+        }
+
+        private static ActionContext ObterActionContext()
+        {
             var request = new Mock<HttpRequest>();
-            request.Setup(x => x.Headers).Returns(new HeaderDictionary() { { "Paginacao",""} });
-            //StringBuilder sb = new StringBuilder();
-            //StringWriter sw = new StringWriter(sb);
-            //HttpResponse response = new HttpResponse(sw);
+            var headerDictionary = new HeaderDictionary { { "Paginacao", "" } };
+            request.Setup(x => x.Headers).Returns(headerDictionary);
+
             var context = new Mock<HttpContext>();
             context.SetupGet(x => x.Request).Returns(request.Object);
-            //context.SetupGet(x => x.Response).Returns(new HttpResponse());
-            ActionContext actionContext = new ActionContext(context.Object, new Microsoft.AspNetCore.Routing.RouteData(), new Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor());
 
-            CandidatoController controller = new CandidatoController(mapper.Object, candidatoService.Object);
-            controller.ControllerContext = new ControllerContext(actionContext);
+            var defaultHttpContext = new DefaultHttpContext();
+            var defaultHttpResponse = new DefaultHttpResponse(defaultHttpContext);
+            context.SetupGet(x => x.Response).Returns(defaultHttpResponse);
 
-            var resultado = controller.Get();
+            var routeData = new RouteData();
+            var controllerActionDescriptor = new ControllerActionDescriptor();
 
-
-
-
+            return new ActionContext(context.Object, routeData, controllerActionDescriptor);
         }
     }
 }
