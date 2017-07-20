06ede8962d59fa441bbc5609affed659b1772012
Glaicon <glaicon@inventti.com.br>
2017-07-20 13:14:47 -0300

    GP: Início da implementação de testes no CandidatoControllerTeste.

--- app/BackEnd/BackEnd.Teste/Unidade/CandidatoControllerTeste.cs
+++ app/BackEnd/BackEnd.Teste/Unidade/CandidatoControllerTeste.cs
@@ -36,26 +36,38 @@ namespace BackEnd.Teste.Unidade
         [Theory]
         [InlineData("", 4)]
         [InlineData("{ Pagina : 1 }", 2)]
-        public void deve_obter_todos_os_candidatos(string paginacao, int candidatosEsperados)
+        public void deve_obter_todos_os_candidatos(string paginacao, int quantidadeCandidatosEsperados)
         {
             controller.ControllerContext = new ControllerContext(ObterActionContext(paginacao));
 
-            var candidatosNoBanco = new List<Candidato>
-            {
-                new Candidato() {Id = 1},
-                new Candidato() {Id = 2},
-                new Candidato() {Id = 3},
-                new Candidato() {Id = 4},
-                new Candidato() {Id = 5},
-                new Candidato() {Id = 6}
-            };
+            var candidatosNoBanco = ObterCandidatos();
             candidatoRepository.Setup(x => x.GetAll()).Returns(candidatosNoBanco);
             candidatoRepository.Setup(x => x.Count()).Returns(candidatosNoBanco.Count);
 
             var resultado = controller.Get() as OkObjectResult;
             var candidatosResumido = ((List<CandidatoResumidoViewModel>)resultado.Value);
-            
-            candidatosResumido.Count().ShouldBeEqualTo(candidatosEsperados);
+
+            candidatosResumido.Count().ShouldBeEqualTo(quantidadeCandidatosEsperados);
+        }
+
+        [Theory]
+        [InlineData(4, "Leticia")]
+        [InlineData(1, "Glaicon")]
+        [InlineData(3, "Carla")]
+        [InlineData(6, "Carol")]
+        public void deve_obter_candidato(int idCandidato, string nomeCandidatoEsperado)
+        {
+            var actionContext = ObterActionContext("");
+            controller.ControllerContext = new ControllerContext(actionContext);
+
+            var candidatosNoBanco = ObterCandidatos();
+            //candidatoRepository.Setup(x => x.GetSingle(It.IsAny<Expression<Func<T, bool>>>, It.IsAny<Expression<Func<T, object>>>)).Returns(candidatosNoBanco);
+            candidatoRepository.Setup(x => x.Count()).Returns(candidatosNoBanco.Count);
+
+            var resultadoObterCandidato = controller.ObterCandidato(idCandidato) as OkObjectResult;
+            var candidato = ((CandidatoViewModel)resultadoObterCandidato.Value);
+
+            candidato.Nome.ShouldBeEqualTo(nomeCandidatoEsperado);
         }
 
         private static ActionContext ObterActionContext(string paginacao)
@@ -75,6 +87,19 @@ namespace BackEnd.Teste.Unidade
             var controllerActionDescriptor = new ControllerActionDescriptor();
 
             return new ActionContext(context.Object, routeData, controllerActionDescriptor);
+        }
+
+        private List<Candidato> ObterCandidatos()
+        {
+            return new List<Candidato>
+            {
+                new Candidato() {Id = 1, Nome="Glaicon"},
+                new Candidato() {Id = 5, Nome="Maria"},
+                new Candidato() {Id = 2, Nome="Janaina"},
+                new Candidato() {Id = 4, Nome="Leticia"},
+                new Candidato() {Id = 3, Nome="Carla"},
+                new Candidato() {Id = 6, Nome="Carol"}
+            };
         }
     }
 }
